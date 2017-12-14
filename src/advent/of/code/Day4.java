11c6b94af61d3c2d949b3476618464708aa62637
danielly <daniel.lycksen@hrmsoftware.se>
2017-12-14 14:34:06 +0100

    Day 4 part 1

--- src/advent/of/code/Day4.java
+++ src/advent/of/code/Day4.java
@@ -1,6 +1,5 @@
 package advent.of.code;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -8,7 +7,7 @@ import java.util.Scanner;
 
 public class Day4 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String input = "aa bb cc dd ee\n" 
                 + "aa bb cc dd aa\n" 
                 + "aa bb cc dd aaa";
