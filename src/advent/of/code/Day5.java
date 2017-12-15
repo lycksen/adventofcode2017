251e785c7fb1d2032609843c15bfa3c0b6d3bb5b
danielly <daniel.lycksen@hrmsoftware.se>
2017-12-15 10:52:56 +0100

    Day 5 part 1

---
+++ src/advent/of/code/Day5.java
@@ -0,0 +1,36 @@
+package advent.of.code;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day5 {
+    
+    public static void main (String[] args) {
+        String input = "0\n"
+                + "3\n"
+                + "0\n"
+                + "1\n"
+                + "-3";
+
+        Scanner scanner = new Scanner(input);
+        List<Integer> ints = new ArrayList<>();
+        while (scanner.hasNextLine()){
+            ints.add(Integer.parseInt(scanner.nextLine()));
+        }
+        int index = 0;
+        int current = ints.get(index);
+        int i = 0;
+        while (true) {
+            ints.set(index, current+1);
+            index = index + current;
+            if (index >= ints.size()) {
+                i++;
+                break;
+            }
+            current = ints.get(index);
+            i++;
+        }
+        System.out.print(i);        
+    }
+}
